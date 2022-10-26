c0792f4337d48b11a53fe10bbdfa7744a2929e0a
ERLMKea <erlm@kea.dk>
2022-10-26 10:05:02 +0200

    Initial commit

---
+++ src/main/java/com/example/studentudvdag01/StudentUdvdag01Application.java
@@ -0,0 +1,13 @@
+package com.example.studentudvdag01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentUdvdag01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentUdvdag01Application.class, args);
+    }
+
+}
